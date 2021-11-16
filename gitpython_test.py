7dab52d15683d6e085890eb8f680f63fe1892fcb
kkado <k.kado.gm@gmail.com>
2021-11-16 10:58:10 +0900

    chk

--- gitpython_test.py
+++ gitpython_test.py
@@ -14,46 +14,47 @@ print("remote_repo.origin: ", remote_repo.origin)
 print("remote_repo.origin.url: ", remote_repo.origin.url)
 # print("remote_repo.origin.url: ", remote_repo.origin.commit)
 
-fetched = remote_repo.origin.fetch()
-print("fetched: ", fetched[0].name)
-for f in fetched:
-    print(f.commit.tree.blobs)
+fetched = remote_repo.origin.fetch()[0]
+print("fetched: ", fetched.name)
+for blob in fetched.commit.tree.blobs:
+    print(blob.name)
+    # if f.commit.tree.blobs.name
 
 
 
-aa
-# true means some modified files exist
-print("is_dirty: ", repo.is_dirty())
+# aa
+# # true means some modified files exist
+# print("is_dirty: ", repo.is_dirty())
 
-heads = repo.heads
-print("heads: ", heads)
-# -> heads:  [<git.Head "refs/heads/main">]
+# heads = repo.heads
+# print("heads: ", heads)
+# # -> heads:  [<git.Head "refs/heads/main">]
 
-print("heads.master.commit: ", heads.main.commit)
-# -> heads.master.commit:  14abcb7e89f17944cea2d70cb9f91038e7694bbd
+# print("heads.master.commit: ", heads.main.commit)
+# # -> heads.master.commit:  14abcb7e89f17944cea2d70cb9f91038e7694bbd
 
-# a tag is a special mark to indicate milestone-like commit
-print(repo.tags)
+# # a tag is a special mark to indicate milestone-like commit
+# print(repo.tags)
 
-blobs = repo.heads.main.commit.tree.blobs
-print("blobs: ", blobs)
-for b in blobs:
-    print(b.name)
-    print(b.data_stream.read())
+# blobs = repo.heads.main.commit.tree.blobs
+# print("blobs: ", blobs)
+# for b in blobs:
+#     print(b.name)
+#     print(b.data_stream.read())
 
-# Git objects consist Blobs, Trees, Commits and Tags.
-# These have common fields: type, size, hexsha and binsha.
-t = heads.main.commit.tree
-print("type: ", t.type, ", size: ", t.size, ", hexsha: ", t.hexsha)
+# # Git objects consist Blobs, Trees, Commits and Tags.
+# # These have common fields: type, size, hexsha and binsha.
+# t = heads.main.commit.tree
+# print("type: ", t.type, ", size: ", t.size, ", hexsha: ", t.hexsha)
 
 
 
-# hcommit = repo.head.commit
-# print(hcommit.diff())                  # diff tree against index
-# print(hcommit.diff('HEAD~1'))          # diff tree against previous tree
-# print(hcommit.diff(None))              # diff tree against working tree
+# # hcommit = repo.head.commit
+# # print(hcommit.diff())                  # diff tree against index
+# # print(hcommit.diff('HEAD~1'))          # diff tree against previous tree
+# # print(hcommit.diff(None))              # diff tree against working tree
 
-# index = repo.index
-# index.diff()                    # diff index against itself yielding empty diff
-# index.diff(None)                # diff index against working copy
-# index.diff('HEAD')      
+# # index = repo.index
+# # index.diff()                    # diff index against itself yielding empty diff
+# # index.diff(None)                # diff index against working copy
+# # index.diff('HEAD')      
