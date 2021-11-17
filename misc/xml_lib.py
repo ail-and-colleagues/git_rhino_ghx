6fcc6cc296ee39d4c20e5e1274b4c789afa2e8d7
kkado <k.kado.gm@gmail.com>
2021-11-17 18:37:10 +0900

    impl class Line_to_Xml_Element

--- misc/xml_lib.py
+++ misc/xml_lib.py
@@ -38,3 +38,38 @@ def fetch_content_recursive(cur, att_key, att_val):
         ret.append(child)
         ret += fetch_content_recursive(child.xpath("./*"), att_key, att_val)
     return ret
+
+
+class Line_to_Xml_Element:
+    def __init__(self):
+        self.is_end = False
+        self.map = list()
+        self.depth = 0
+        self.element_idx = -1
+        self.depth_element_idx = dict()
+        self.br_count = 0
+    def start(self, tag, attrib):
+        self.depth += 1
+        self.element_idx += 1
+        # print("start: ", tag, attrib)
+        self.depth_element_idx[self.depth] = self.element_idx
+        # print("depth_element_idx: ", self.depth_element_idx)
+    def end(self, tag):
+        self.is_end = True
+        # print("end: ", tag)
+    def data(self, data):
+        self.br_count = data.count("\n")
+        # print("data: ", data, ", br_count: ", self.br_count )
+        cur_idx = self.depth_element_idx[self.depth]
+        self.map += [cur_idx] * self.br_count
+        if self.is_end:
+            self.depth += -1
+            self.is_end = False
+    def close(self):
+        # Add idx(s) because data() does not be called on the last line.
+        cur_idx = self.depth_element_idx[self.depth]
+        self.map += [cur_idx] * self.br_count
+        # Insert "-1" for the skipped first line "<?xml version=~".
+        self.map = [-1] + self.map
+        # print("map: ", self.map)
+        return self.map
